2b38a630502fe49ced4d0ed790697e2ae293e57e
Nay Htet Aung <nayhtetaung201733@gmail.com>
2022-02-25 16:48:09 +0630

    Job search API Route

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -1,31 +1,51 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import {API_URL } from '../../lib/api'
-
-export async function getData() {
-  try{
-    const data = await fetch(`${API_URL}?limit=50`);
-    const jsonData = await data.json();
-    return jsonData
-  }catch(err){
-    return new Error('Error fetching data')
-  }
-  
+import type { NextApiRequest, NextApiResponse } from "next";
+import { API_URL } from "../../lib/api";
+interface RequestRoute extends NextApiRequest {
+  limit?: string;
+  company_name?: string;
+  search?: string;
+  category?: string;
 }
-
+export async function getData() {
+  try {
+    const data = await fetch(`${API_URL}?limit=1`);
+    const jsonData = await data.json();
+    return jsonData;
+  } catch (err) {
+    return new Error("Error fetching data");
+  }
+}
 export default async function handler(
-  _: NextApiRequest,
+  request: RequestRoute,
   res: NextApiResponse
 ) {
-  try{
-    const jsonData = await getData();
-    res.status(200).json(jsonData)
+  const req = JSON.parse(request.body);
+
+  if (request.method === "POST") {
+    let query = "";
+    if (req.category) {
+      query += `&category=${req.category}`;
+    }
+    if (req.company_name) {
+      query += `&company_name=${req.company_name.replace(" ", "+")}`;
+    }
+    if (req.search) {
+      query += `&search=${req.search}`;
+    }
+    if (req.limit) {
+      query += `&limit=${req.limit}`;
+    }
+    if (query.length) {
+      query = query.substring(1);
+    }
+    try {
+      const data = await fetch(`${API_URL}?${query}`);
+      const jsonData = await data.json();
+      const dataResource = await jsonData.jobs;
+      res.status(200).json(dataResource);
+    } catch (err) {
+      res.status(500).json({ error: err });
+    }
   }
-  catch(err){
-    res.status(500).json({error: err})
-  }
-
- 
-
-
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,26 @@ import { GithubJob } from "../lib/api"
 import { getData } from "./api";
 import { Layout } from "../components/layout";
 import { JobCard } from "../components/jobs";
+import { useEffect } from "react";
 interface HomeProps {
   jobs: GithubJob[]
 }
 const Home: NextPage<HomeProps> = (props) => {
-
+  const searchHandler = () =>{
+    fetch('/api',{
+      method: 'POST',
+      body:JSON.stringify({
+        limit:10,
+        search:'well-being conversation'
+      })
+    })
+    .then((res) => res.json())
+      .then(console.log)
+      .catch(console.log);
+  }
+  useEffect(() => {
+    searchHandler();
+  }, []);
   return <Layout title="Home">{props.jobs.map((job) => (
     <JobCard key={job.id} {...job} />
   ))}</Layout>;
@@ -16,8 +31,6 @@ export async function getServerSideProps() {
   try {
     const dataJson = await getData();
     const jobs = await dataJson.jobs;
-    console.log(jobs);
-    
     return {
       props: {
         jobs: jobs
